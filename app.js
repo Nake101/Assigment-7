3fc89b9101b01913442f7aff98a022926f7ea31b
NAIK RAHIS <143823935+Nake101@users.noreply.github.com>
2023-10-09 21:40:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+// var clickableElement = document.getElementById('clickable-element');
+// clickableElement.addEventListener('click', 
+// function () {
+//     console.log('Element clicked! You can perform your action here.');
+// });
+
+
+// var elementToHide = document.getElementById('element-to-hide');
+// var triggerElement = document.getElementById('trigger-element');
+
+
+// triggerElement.addEventListener('click', function () {
+    
+//     elementToHide.style.display = 'none';
+// });
+
+
+
+
+
+        
+        // const scrollToTopLink = document.getElementById('scroll-to-top-link');
+        // scrollToTopLink.addEventListener('click',
+        //  function () {
+        //     window.scrollTo({
+        //         top: 0,
+        //         behavior: 'smooth'
+        //     });
+        // });
+
+
+        
+
+        // var playSoundLink = document.getElementById('play-sound-link');
+        // var audio = document.getElementById('myAudio');
+        // playSoundLink.addEventListener('click',
+        //  function (event) {
+        //     event.preventDefault();
+        //     audio.play();
+        // });
+    
+
+// function Cong(){
+//     alert("welcom here")
+// }
+
+
+
+        
+    //    var elementToHover = document.getElementById('element-to-hover');
+
+    //     elementToHover.addEventListener('mouseover', function () {
+            
+    //         alert('Mouse moved over the element!');
+    //     });
+
+
+
+      
+    //   const elementToHide = document.getElementById('element-to-hide');
+    //   elementToHide.addEventListener('mouseout', function () {
+    //       elementToHide.style.display = 'none'; 
+    //   });
